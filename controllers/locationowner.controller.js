95b6cb170329f72e14994467f41266f73beeb2f9
Krishankant Karadia <krishankantkaradia8@gmail.com>
2025-11-17 15:32:48 +0530

    Monday commit

--- controllers/locationowner.controller.js
+++ controllers/locationowner.controller.js
@@ -4,42 +4,6 @@ import { STATUS_CODES } from '../helpers/constants.js';
 
 export const LocationownerController = {};
 
-// LocationownerController.getNearbySaloons = async (req, res, next) => {
-//   try {
-//     const { lat, long } = req.query;
-
-//     if (!lat || !long) {
-//       return next(new AppError('Latitude and Longitude are required', STATUS_CODES.BAD_REQUEST));
-//     }
-
-//     const latitude = parseFloat(lat);
-//     const longitude = parseFloat(long);
-//     const radiusInMeters = 5 * 1000; 
-
-//     const nearbyLocations = await Location.find({
-//       geoLocation: {
-//         $near: {
-//           $geometry: {
-//             type: 'Point',
-//             coordinates: [longitude, latitude] 
-//           },
-//           $maxDistance: radiusInMeters
-//         }
-//       }
-//     }).populate('saloon');
-
-//     return res.status(STATUS_CODES.OK).json({
-//       success: true,
-//       count: nearbyLocations.length,
-//       data: nearbyLocations
-//     });
-
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-
-
 LocationownerController.getNearbySaloons = async (req, res, next) => {
   try {
     const { lat, long } = req.query;
@@ -50,27 +14,19 @@ LocationownerController.getNearbySaloons = async (req, res, next) => {
 
     const latitude = parseFloat(lat);
     const longitude = parseFloat(long);
-    const radiusInMeters = 2 * 1000; // 5 km
+    const radiusInMeters = 5 * 1000;
 
     const nearbyLocations = await Location.find({
       geoLocation: {
         $near: {
           $geometry: {
-            type: "Point",
+            type: 'Point',
             coordinates: [longitude, latitude]
           },
           $maxDistance: radiusInMeters
         }
       }
-    })
-      .populate({
-        path: "saloon",
-        select: "name logo rating city status"
-      })
-      .populate({
-        path: "owner",
-        select: "name mobile email"
-      });
+    }).populate('saloon');
 
     return res.status(STATUS_CODES.OK).json({
       success: true,
@@ -82,6 +38,9 @@ LocationownerController.getNearbySaloons = async (req, res, next) => {
     next(err);
   }
 };
+
+
+
 
 
 LocationownerController.getAllSaloons = async (req, res, next) => {
