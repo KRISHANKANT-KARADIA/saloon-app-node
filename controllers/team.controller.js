21968e4c86ff5d4cc5682b54ee812be14fd718cd
Pavan Kumar <pavankumar10891@gmail.com>
2025-11-25 15:44:17 +0530

    Add Team comminitss

--- controllers/team.controller.js
+++ controllers/team.controller.js
@@ -113,7 +113,8 @@ import TeamMember from '../models/teamMember.model.js';
 import Saloon from '../models/saloon.model.js';
 import Appointment from '../models/appointment.model.js';
 import ownerModel from '../models/owner.model.js';
-import { sendNotification } from './../helpers/sendNotification';
+// import { sendNotification } from './../helpers/sendNotification.js';
+import { sendNotification } from '../helpers/sendNotification.js';
 export const addTeamMember = async (req, res, next) => {
   try {
     const ownerId = res.locals.user.id;
