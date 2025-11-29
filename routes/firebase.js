ac4500ac6aa96ee16f33c3ac2f41b2e057517b36
Pavan Kumar <pavankumar10891@gmail.com>
2025-11-29 11:11:18 +0530

    hhsagdhsa v

--- routes/firebase.js
+++ routes/firebase.js
@@ -1,7 +1,6 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
-const serviceAccount = require('./serviceAccountKey.json');
-
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
